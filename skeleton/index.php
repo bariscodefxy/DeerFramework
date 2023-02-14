283afe5a494b5be2d5643cf7cff5058cac1965e3
bariscodefxy <bariscodefx@protonmail.com>
2023-02-14 09:04:09 +0300

    skeleton: index.php: some updates

--- skeleton/index.php
+++ skeleton/index.php
@@ -4,9 +4,8 @@ require dirname(__DIR__, 1) . "/vendor/autoload.php";
 use Deer\Deer;
 $deer = new Deer();
 $router = $deer->getRouter();
-$router->get('/', function()
-{
-	return 'lol';
+$router->get('/', function(){
+	return 'Hello World <a href="/users">Go to UserController</a>';
 });
 $router->controller('/users', 'UserController', ['before' => "UserMiddleware"]);
 $deer->do();
